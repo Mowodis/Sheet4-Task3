2eb73ceb8551e47b219d63ff7732b4dffc02eb01
Marcel Heine <M_Heine@web.de>
2022-11-17 22:30:30 +0100

    Added 3 example_graphs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -30,14 +30,21 @@ def printMatrix(matrix: Array[Array[Int]]) = {
 // each vertex has been assighed a number >= 1, increasing
 def printSCCs(matrix: Array[Array[Int]], name: String) : Unit = {
     val newSCCs = findSCCs(matrix)
+    
+    println("")
     println("SCCs of graph: " + name)
     
     for (i <- 0 to newSCCs.length - 1)
         print(" ( ")
         
         for (j <- 0 to newSCCs(i).length - 1) {
-            print(newSCCs(i)(j) + 1)
-            print(" ")
+            // start a new paragraph after 20 entries
+            if (j % 20 == 19 && j != (newSCCs(i).length - 1)) {
+                println(newSCCs(i)(j) + 1 + " ")
+                print("   ")
+            } else {
+                print(newSCCs(i)(j) + 1 + " ")
+            }
         }
         println(")")
 }
@@ -47,7 +54,9 @@ def main(args: Array[String]): Unit = {
     // load test templates 
     val big_graph = createMatrix(1000,"src/main/resources/big_graph.csv")
     val small_graph = createMatrix(7,"src/main/resources/small_graph.csv")
-    
-    println("SCCs: " + findSCCs(small_graph).toString())
-    printSCCs(small_graph, "small_graph")    
+    val fullyConnected_graph = createMatrix(5,"src/main/resources/fullyConnected_graph.csv")
+    val simpleCycle_graph = createMatrix(3,"src/main/resources/simpleCycle_graph.csv")
+    val twoCycle_graph = createMatrix(6,"src/main/resources/twoCycle_graph.csv")
+
+    printSCCs(big_graph, "big_graph")    
 }   
